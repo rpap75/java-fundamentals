a72ad4dfaf2fa12bb1060fa0a8cceb2c21249dff
Ryan Papsin <113944887+rpap75@users.noreply.github.com>
2023-01-12 17:28:01 -0700

    Update LibraryTest.java

--- basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
@@ -52,4 +52,40 @@ int[] expectedArray =  (testArray[3]);
         assertNotNull(result);
         assertEquals(expectedArray, (result));
     }
+      @Test
+    void testWeatherData() {
+        Library sut = new Library();
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        assert(sut.weatherData(weeklyMonthTemperatures).equals("51 52 53 54 55 56 57 58 59 60 61 62 64 65 66 70 71 72 "));
+    }
+
+    @Test
+    void testTally() {
+        Library sut = new Library();
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+        String elected = sut.tally(votes);
+        assert(elected.equals("Bush"));
+        System.out.println("\n~~ Bush vs. Hedge vs. Shrub ~~");
+        System.out.println(elected + " received the most votes!");
+    }
+
+    class AppTest {
+        @Test void appHasAGreeting() {
+            App classUnderTest = new App();
+            assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        }
 }
